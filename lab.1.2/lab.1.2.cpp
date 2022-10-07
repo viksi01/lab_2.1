dd4fe440a5b62d7e561c2d12854dab2e6aabfc7f
viksi01 <victoria.pyatnicka@gmail.com>
2022-10-07 12:37:38 +0300

    commit1

---
+++ lab.1.2/lab.1.2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double a;
+	double z1;
+	double z2;
+	cout << "a = "; cin >> a;
+	z1 = (pow((1 + a + a * a) / (2 * a + a * a) + (2 - (1 - a + a * a) / (2 * a - a * a)), -1) * (5 - 2 * a * a));
+	z2 = (4 - a * a) / 2;
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
